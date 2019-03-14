f4223553c9f0abc5d050dc9109ddd5f3696094a0
Radliński Ignacy <radlinskiignacy@gmail.com>
2019-03-14 17:25:56 +0100

    Booleans & Null (#17)
    * add boolean literals evaluation
    
    * update referencing boolean and null values

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,15 @@ import (
 	"github.com/radlinskii/interpreter/object"
 )
 
+var (
+	// TRUE is a single object that all the appeareances of boolean nodes with value "true" will point to.
+	TRUE = &object.Boolean{Value: true}
+	// FALSE is a single object that all the appeareances of boolean nodes with value "false" will point to.
+	FALSE = &object.Boolean{Value: false}
+	// NULL is a single object that all the appeareances of nodes without a value will point to.
+	NULL = &object.Null{}
+)
+
 // Eval evaluates the program
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
@@ -16,6 +25,11 @@ func Eval(node ast.Node) object.Object {
 	//Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.BooleanLiteral:
+		if node.Value {
+			return TRUE
+		}
+		return FALSE
 	}
 
 	return nil
